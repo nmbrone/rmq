d51030b399df512b1ccc748c3e4a194200261901
Sergey Snozyk <sergey.snozyk@gmail.com>
2019-12-18 15:03:58 +0100

    fix test

--- test/rmq/connection_test.exs
+++ test/rmq/connection_test.exs
@@ -4,20 +4,22 @@ defmodule RMQ.ConnectionTest do
 
   test "accepts options via configuration" do
     Application.put_env(:rmq, :connection, uri: rabbit_uri(), virtual_host: "not_exist")
-    assert {:ok, _pid} = RMQ.Connection.start_link()
+    assert {:ok, pid} = RMQ.Connection.start_link()
     assert {:error, :not_connected} = RMQ.Connection.get_connection()
+    GenServer.stop(pid)
     Application.delete_env(:rmq, :connection)
   end
 
   test "accepts options via `start_link/1`" do
     Application.put_env(:rmq, :connection, virtual_host: "not_exist")
-    assert {:ok, _pid} = RMQ.Connection.start_link(uri: rabbit_uri(), virtual_host: "/")
+    assert {:ok, pid} = RMQ.Connection.start_link(uri: rabbit_uri(), virtual_host: "/")
     assert {:ok, _conn} = RMQ.Connection.get_connection()
+    GenServer.stop(pid)
     Application.delete_env(:rmq, :connection)
   end
 
   test "reconnects in case of losing connection" do
-    {:ok, _pid} = RMQ.Connection.start_link(uri: rabbit_uri(), reconnect_interval: 100)
+    {:ok, pid} = RMQ.Connection.start_link(uri: rabbit_uri(), reconnect_interval: 100)
     {:ok, conn} = RMQ.Connection.get_connection()
     # Process.exit(conn.pid, "Good night!")
     AMQP.Connection.close(conn)
@@ -25,5 +27,6 @@ defmodule RMQ.ConnectionTest do
     assert {:error, :not_connected} = RMQ.Connection.get_connection()
     Process.sleep(200)
     assert {:ok, %AMQP.Connection{}} = RMQ.Connection.get_connection()
+    GenServer.stop(pid)
   end
 end
